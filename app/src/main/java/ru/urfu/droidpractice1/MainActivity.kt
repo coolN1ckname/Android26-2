d8b15a835e23af07c0f20b7cf8fe6b6bd27ad7e3
coolN1ckname <nikitakurmanov4@gmail.com>
2026-09-21 07:36:06 +0500

    second half homework

--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -1,46 +1,51 @@
 package ru.urfu.droidpractice1
 
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.service.autofill.OnClickAction
 import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import ru.urfu.droidpractice1.content.MainActivityScreen
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
+import ru.urfu.droidpractice1.content.MainActivityScreen
 import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.ui.graphics.Color
-import android.content.Intent
-import androidx.compose.ui.platform.LocalContext
-
 
 const val BOTTOM_PADDING = 16
 const val HEADER_FONT_SIZE = 20
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MainActivityScreen()
         }
@@ -49,80 +54,239 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
-fun ComposeArticleScreen(modifier: Modifier) {
+fun ComposeArticleScreen(modifier: Modifier = Modifier) {
 
     val context = LocalContext.current
 
+    val articlePreferences = context.getSharedPreferences(
+        "article_preferences",
+        Context.MODE_PRIVATE
+    )
+
+    val settingsPreferences = context.getSharedPreferences(
+        "settings",
+        Context.MODE_PRIVATE
+    )
+
+    var likes by remember {
+        mutableIntStateOf(
+            articlePreferences.getInt("likes", 0)
+        )
+    }
+
+    var dislikes by remember {
+        mutableIntStateOf(
+            articlePreferences.getInt("dislikes", 0)
+        )
+    }
+
+    // 0 = нет оценки, 1 = лайк, -1 = дизлайк
+    var userVote by remember {
+        mutableIntStateOf(
+            articlePreferences.getInt("userVote", 0)
+        )
+    }
+
+    var isRead by remember {
+        mutableStateOf(
+            settingsPreferences.getBoolean("isRead", false)
+        )
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+
+        if (result.resultCode == RESULT_OK) {
+            isRead = result.data?.getBooleanExtra(
+                "isRead",
+                false
+            ) ?: false
+        }
+    }
+
     DroidPractice1Theme {
-        Scaffold(
-        ) { innerPadding ->
+
+        Scaffold { innerPadding ->
 
             Column(
-                modifier = Modifier
+                modifier = modifier
                     .padding(innerPadding)
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
                     .padding(16.dp)
             ) {
 
-                Spacer(modifier = Modifier.height(16.dp))
+//                Spacer(
+//                    modifier = Modifier.height(16.dp)
+//                )
 
                 Text(
-                    text = " ДАСТ 200",
+                    text = "Стоковые изображения",
                     style = MaterialTheme.typography.bodyLarge,
                     fontSize = HEADER_FONT_SIZE.sp,
-                    modifier = Modifier
-                        .padding(bottom = BOTTOM_PADDING.dp)
+                    modifier = Modifier.padding(
+                        bottom = BOTTOM_PADDING.dp
+                    )
                 )
+                //Лайки и дизлайки
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    //кнопка лайка
+                    Button(
+                        onClick = {
 
-                //Лайки и дизы
-                Button(
-                    onClick = {
-                        TODO()
-                    },
-                    modifier = Modifier
+                            when (userVote) {
+                                0 -> {
+                                    likes++
+                                    userVote = 1
+                                }
 
-                ){
-                    Text("")
+                                1 -> {
+                                    likes--
+                                    userVote = 0
+                                }
+
+                                -1 -> {
+                                    dislikes--
+                                    likes++
+                                    userVote = 1
+                                }
+                            }
+
+                            articlePreferences
+                                .edit()
+                                .putInt("likes", likes)
+                                .putInt("dislikes", dislikes)
+                                .putInt("userVote", userVote)
+                                .apply()
+                        },
+
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor =
+                                if (userVote == 1) {
+                                    Color.Green
+                                } else {
+                                    Color.Gray
+                                },
+                            contentColor = Color.Black
+                        )
+                    ) {
+                        Text("👍 $likes")
+                    }
+
+                    Spacer(
+                        modifier = Modifier
+                            .height(6.dp)
+                            .padding(2.dp)
+                    )
+                    // кноака диза
+                    Button(
+                        onClick = {
+
+                            when (userVote) {
+                                0 -> {
+                                    dislikes++
+                                    userVote = -1
+                                }
+
+                                1 -> {
+                                    likes--
+                                    dislikes++
+                                    userVote = -1
+                                }
+
+                                -1 -> {
+                                    dislikes--
+                                    userVote = 0
+                                }
+                            }
+
+                            articlePreferences
+                                .edit()
+                                .putInt("likes", likes)
+                                .putInt("dislikes", dislikes)
+                                .putInt("userVote", userVote)
+                                .apply()
+                        },
+
+
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor =
+                                if (userVote == -1) {
+                                    Color.Red
+                                } else {
+                                    Color.Gray
+                                },
+                            contentColor = Color.Black
+                        )
+                    ) {
+                        Text("👎 $dislikes")
+                    }
                 }
+                Spacer(
+                    modifier = Modifier.height(16.dp)
+                )
 
                 GlideImage(
                     model = "https://cs10.pikabu.ru/post_img/big/2018/09/03/6/153596777311515846.jpg",
                     contentDescription = "Прикольно, что есть описание картинок",
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(bottom = BOTTOM_PADDING.dp)
+                        .padding(
+                            bottom = BOTTOM_PADDING.dp
+                        )
                 )
 
                 Text(
                     text = """
-                        Это текст статьи.
-
-                        Здесь может быть первый абзац, второй абзац
-                        и другая информация.
-
-                        Вся разметка этой статьи создаётся
-                        непосредственно с помощью Jetpack Compose.
+                        Стоковые изображения - это готовые фотографии и иллюстрации, которые можно использовать в проектах, презентациях и публикациях.
+                         
+                        Они помогают быстро подобрать подходящий визуальный материал без необходимости самостоятельно организовывать съёмку.
+                        
+                        Это невероятно удобно, к тому же среди них можно найти довольно много, так называемых, скрытых гемов, что на наш взгляд, повышает их значимость.
+                        
+                        Зачастую на них присутствуют вотермарки, но это довольно малая цена за их использование
                     """.trimIndent(),
+
                     style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier
-                        .padding(bottom = BOTTOM_PADDING.dp)
+
+                    modifier = Modifier.padding(
+                        bottom = BOTTOM_PADDING.dp
+                    )
                 )
 
                 Button(
                     onClick = {
-                        val intent = Intent(context, SecondActivity::class.java)
-                        context.startActivity(intent)
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Green,
-                        contentColor = Color.Gray
-                    )
-                ){
-                    Text("абоба $")
 
+                        val intent = Intent(
+                            context,
+                            SecondActivity::class.java
+                        )
+
+                        launcher.launch(intent)
+                    },
+
+                    modifier = Modifier.fillMaxWidth(),
+
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor =
+                            if (isRead) {
+                                Color.Green
+                            } else {
+                                Color.Gray
+                            },
+                        contentColor = Color.Black
+                    )
+                ) {
+                    Text(
+                        if (isRead) {
+                            "Следующая статья на Вьюхе (прочитано)"
+                        } else {
+                            "Следующая статья на Вьюхе"
+                        }
+                    )
                 }
             }
         }
--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -1,5 +1,6 @@
 package ru.urfu.droidpractice1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.bumptech.glide.Glide
@@ -15,6 +16,12 @@ class SecondActivity : ComponentActivity() {
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val preferences = getSharedPreferences("settings", MODE_PRIVATE)
+
+        // Восстанавленниие сохранённого результата
+        binding.readSwitch.isChecked =
+            preferences.getBoolean("isRead", false)
+
         binding.backButton.setOnClickListener {
             finish()
         }
@@ -22,5 +29,19 @@ class SecondActivity : ComponentActivity() {
         Glide.with(this)
             .load("https://www.cossa.ru/upload/iblock/d55/tyz.jpg")
             .into(binding.articleImage)
+
+        binding.readSwitch.setOnCheckedChangeListener { _, isChecked ->
+
+            // Сохраниение результата
+            preferences.edit()
+                .putBoolean("isRead", isChecked)
+                .apply()
+
+            // Передача результата обратно
+            val resultIntent = Intent()
+            resultIntent.putExtra("isRead", isChecked)
+
+            setResult(RESULT_OK, resultIntent)
+        }
     }
 }
